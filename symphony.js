487cafe88cdf86d5a746887202470ac2dab7ed03
Tally Lassiter <whistling.herring@gmail.com>
2013-11-16 18:10:02 -0500

    JS prep

---
+++ symphony.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#top3summary img").click(function() {
+		$("#top3summary").toggle();
+		$("#no1details").toggle();
+	})
+});
+
+function expandTop3(album) {
+	
+}
