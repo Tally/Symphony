3083ba4d80c754c9fc4633c10c60379f0aa46397
Tally Lassiter <whistling.herring@gmail.com>
2013-11-28 17:25:30 -0500

    basic JS

--- symphony.js
+++ symphony.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-	$("#top3summary img").click(function() {
+	$("#no1 img").click(function() {
 		$("#top3summary").toggle();
 		$("#no1details").toggle();
+	})
+	$("#no2 img").click(function() {
+		$("#top3summary").toggle();
+		$("#no2details").toggle();
+	})
+	$("#no3 img").click(function() {
+		$("#top3summary").toggle();
+		$("#no3details").toggle();
 	})
 });
 
 function expandTop3(album) {
-	
+
 }
